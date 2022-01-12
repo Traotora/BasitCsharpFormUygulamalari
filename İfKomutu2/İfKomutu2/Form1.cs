4b830daa76b3e90a369e386c52980abcc22638e5
Mahir TAŞTEMÜR <70233223+Traotora@users.noreply.github.com>
2022-01-12 12:26:23 +0300

    Add files via upload

---
+++ İfKomutu2/İfKomutu2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace İfKomutu2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int ilksayi, ikincisayi;
+            ilksayi = 10;
+            ikincisayi = 7;
+            bool sonuc = false;
+            sonuc = (ilksayi > ikincisayi);
+            if(sonuc)
+            {
+                MessageBox.Show("İlk sayı büyüktür");
+
+            }
+            else
+            {
+                MessageBox.Show("İkinci sayı büyüktür");
+            }
+
+
+        }
+    }
+}
